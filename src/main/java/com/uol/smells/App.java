f2996a2cf34cc20b480612150d55c0b0836be6d6
rferreira <rferreira@uolinc.com>
2014-09-30 14:18:06 -0300

    Commit inicial do projeto smells account.

---
+++ src/main/java/com/uol/smells/App.java
@@ -0,0 +1,13 @@
+package com.uol.smells;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
